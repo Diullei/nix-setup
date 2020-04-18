148cf52d16e2d9a3e9282ae2f0e48446da7200dc
Diullei <diullei@gmail.com>
2020-04-18 09:37:56 +0000

    update - Sat Apr 18 09:37:56 UTC 2020

--- 03-encryption-and-LVM.sh
+++ 03-encryption-and-LVM.sh
@@ -3,11 +3,11 @@
 #   amount of RAM on this machine eg. 32
 
 # -- You will be asked to enter your passphrase - DO NOT FORGET THIS
-cryptsetup luksFormat "$0"
+cryptsetup luksFormat $0
 
 # -- Decrypt the encrypted partition and call it nixos-enc. The decrypted partition
 # -- will get mounted at /dev/mapper/nixos-enc
-cryptsetup luksOpen "$0" nixos-enc
+cryptsetup luksOpen $0 nixos-enc
     
 # -- Create the LVM physical volume using nixos-enc
 pvcreate /dev/mapper/nixos-enc 
@@ -17,7 +17,7 @@ vgcreate nixos-vg /dev/mapper/nixos-enc
 
 # -- Create a swap partition that is 16G in size - the amount of RAM on this machine
 # -- Volume is labeled "swap"'
-lvcreate -L "$1G" -n swap nixos-vg
+lvcreate -L $1G -n swap nixos-vg
 
 # -- Create a logical volume for our root filesystem from all remaining free space.
 # -- Volume is labeled "root"
